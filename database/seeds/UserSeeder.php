9892d39e2abe487d8114990b499bc6f7736f2a2d
yogapratama24 <ypramsatya@gmail.com>
2021-12-03 04:38:53 +0700

    seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -51,120 +51,120 @@ class UserSeeder extends Seeder
             'password' => bcrypt('members12345'),
         ]);
         $members->assignRole('members');
-        DB::table('categories')->insert([
-            [
-                'name' => 'Breakfast'
-            ],
-            [
-                'name' => 'Beverage'
-            ],
-            [
-                'name' => 'Lunch'
-            ],
-            [
-                'name' => 'Drinks'
-            ],
-            [
-                'name' => 'Dinner'
-            ],
-            [
-                'name' => 'Fruits'
-            ],
-            [
-                'name' => 'Snacks'
-            ]
-        ]);
+        // DB::table('categories')->insert([
+        //     [
+        //         'name' => 'Breakfast'
+        //     ],
+        //     [
+        //         'name' => 'Beverage'
+        //     ],
+        //     [
+        //         'name' => 'Lunch'
+        //     ],
+        //     [
+        //         'name' => 'Drinks'
+        //     ],
+        //     [
+        //         'name' => 'Dinner'
+        //     ],
+        //     [
+        //         'name' => 'Fruits'
+        //     ],
+        //     [
+        //         'name' => 'Snacks'
+        //     ]
+        // ]);
 
-        $faker = Faker::create('id_ID');
-        $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
-        DB::table('inventories')->insert([
-            'ingredients' => 'Sasa',
-            'stock_quantity' => 100000,
-            'alert_quantity' => 1000,
-            'unit' => 'gram'
-        ]);
-        $voucher = DB::table('vouchers')->insert([
-            'name' => 'super hemat',
-            'discount' => 10,
-            'status' => 'Active'
-        ]);
-        for ($i = 1; $i <= 100; $i++) {
-            $category = $faker->numberBetween(1, 7);
-            $menu = $faker->numberBetween(1, 50);
-            $role = $faker->numberBetween(1, 4);
-            $user = DB::table('users')->insertGetId([
-                'name' => $faker->name(),
-                'email' => $faker->unique()->email,
-                'phone_number' => $faker->phoneNumber(),
-                'email_verified_at' => Carbon::now(),
-                'password' => bcrypt('secret12345')
-            ]);
-            DB::table('model_has_roles')->insert([
-                'role_id' => $role,
-                'model_type' => 'App\User',
-                'model_id' => $user,
-            ]);
+        // $faker = Faker::create('id_ID');
+        // $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
+        // DB::table('inventories')->insert([
+        //     'ingredients' => 'Sasa',
+        //     'stock_quantity' => 100000,
+        //     'alert_quantity' => 1000,
+        //     'unit' => 'gram'
+        // ]);
+        // $voucher = DB::table('vouchers')->insert([
+        //     'name' => 'super hemat',
+        //     'discount' => 10,
+        //     'status' => 'Active'
+        // ]);
+        // for ($i = 1; $i <= 100; $i++) {
+        //     $category = $faker->numberBetween(1, 7);
+        //     $menu = $faker->numberBetween(1, 50);
+        //     $role = $faker->numberBetween(1, 4);
+        //     $user = DB::table('users')->insertGetId([
+        //         'name' => $faker->name(),
+        //         'email' => $faker->unique()->email,
+        //         'phone_number' => $faker->phoneNumber(),
+        //         'email_verified_at' => Carbon::now(),
+        //         'password' => bcrypt('secret12345')
+        //     ]);
+        //     DB::table('model_has_roles')->insert([
+        //         'role_id' => $role,
+        //         'model_type' => 'App\User',
+        //         'model_id' => $user,
+        //     ]);
 
-            $id = DB::table('menus')->insertGetId([
-                'name' => $faker->foodName(),
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, deserunt?',
-                'hpp' => $faker->numberBetween(15000, 35000),
-                'price' => $faker->numberBetween(20000, 50000),
-                // 'image' => $faker->imageUrl(120, 120),
-                'image' => 'uploads/menus/image_example.jpg',
-                'discount' => $faker->numberBetween(1, 10),
-                'category_id' => $category
-            ]);
+        //     $id = DB::table('menus')->insertGetId([
+        //         'name' => $faker->foodName(),
+        //         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, deserunt?',
+        //         'hpp' => $faker->numberBetween(15000, 35000),
+        //         'price' => $faker->numberBetween(20000, 50000),
+        //         // 'image' => $faker->imageUrl(120, 120),
+        //         'image' => 'uploads/menus/image_example.jpg',
+        //         'discount' => $faker->numberBetween(1, 10),
+        //         'category_id' => $category
+        //     ]);
 
-            $table = DB::table('tables')->insertGetId([
-                'name' => "A$i"
-            ]);
+        //     $table = DB::table('tables')->insertGetId([
+        //         'name' => "A$i"
+        //     ]);
 
-            DB::table('inventory_menus')->insert([
-                'inventory_id' => 1,
-                'menu_id' => $id,
-                'consumption' => 100
-            ]);
+        //     DB::table('inventory_menus')->insert([
+        //         'inventory_id' => 1,
+        //         'menu_id' => $id,
+        //         'consumption' => 100
+        //     ]);
 
-            $sale = DB::table('sales')->insertGetId([
-                'table_id' => $table,
-                'user_id' => $user,
-                'voucher_id' => $voucher,
-                'customer_name' => $faker->name(),
-                'customer_phone' => $faker->phoneNumber(),
-                'total_hpp' => $faker->numberBetween(30000, 50000),
-                'total_price' => $faker->numberBetween(50000, 80000),
-                'total_vat' => $faker->numberBetween(1, 10),
-                'total_vatprice' => $faker->numberBetween(80000, 95000),
-                'total_received' => $faker->numberBetween(100000, 150000),
-                'change' => $faker->numberBetween(5000, 15000),
-                'payment_type' => $faker->randomElement(['cash', 'bank_transfer', 'payment_card']),
-                'sale_status' => 'paid',
-                'created_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
-                'updated_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null)
-            ]);
+        //     $sale = DB::table('sales')->insertGetId([
+        //         'table_id' => $table,
+        //         'user_id' => $user,
+        //         'voucher_id' => $voucher,
+        //         'customer_name' => $faker->name(),
+        //         'customer_phone' => $faker->phoneNumber(),
+        //         'total_hpp' => $faker->numberBetween(30000, 50000),
+        //         'total_price' => $faker->numberBetween(50000, 80000),
+        //         'total_vat' => $faker->numberBetween(1, 10),
+        //         'total_vatprice' => $faker->numberBetween(80000, 95000),
+        //         'total_received' => $faker->numberBetween(100000, 150000),
+        //         'change' => $faker->numberBetween(5000, 15000),
+        //         'payment_type' => $faker->randomElement(['cash', 'bank_transfer', 'payment_card']),
+        //         'sale_status' => 'paid',
+        //         'created_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
+        //         'updated_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null)
+        //     ]);
 
-            DB::table('sale_details')->insertGetId([
-                'sale_id' => $sale,
-                'menu_id' => $id,
-                'menu_name' => $faker->foodName(),
-                'menu_price' => $faker->numberBetween(40000, 60000),
-                'quantity' => $faker->numberBetween(1, 3),
-                'menu_discount' => $faker->numberBetween(2, 4),
-                'note' => 'ayamnya banyakin',
-                'status' => 'confirm',
-                'created_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
-                'updated_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null)
-            ]);
+        //     DB::table('sale_details')->insertGetId([
+        //         'sale_id' => $sale,
+        //         'menu_id' => $id,
+        //         'menu_name' => $faker->foodName(),
+        //         'menu_price' => $faker->numberBetween(40000, 60000),
+        //         'quantity' => $faker->numberBetween(1, 3),
+        //         'menu_discount' => $faker->numberBetween(2, 4),
+        //         'note' => 'ayamnya banyakin',
+        //         'status' => 'confirm',
+        //         'created_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
+        //         'updated_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null)
+        //     ]);
 
-            DB::table('suppliers')->insert([
-                'date' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
-                'name' => $faker->company(),
-                'total' => $faker->numberBetween(500000, 1000000),
-                'user_id' => $user,
-                'created_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
-                'updated_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null)
-            ]);
-        }
+        // DB::table('suppliers')->insert([
+        //     'date' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
+        //     'name' => $faker->company(),
+        //     'total' => $faker->numberBetween(500000, 1000000),
+        //     'user_id' => $user,
+        //     'created_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null),
+        //     'updated_at' => $faker->dateTimeInInterval($startDate = '0 years', $interval = '- 120 days', $timezone = null)
+        // ]);
+        // }
     }
 }
